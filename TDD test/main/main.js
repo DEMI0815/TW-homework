ca03a1fd1cbd0ecc1c27e1de8b4387912646a956
DEMI0815 <807205251@qq.com>
2019-03-22 11:14:52 +0800

    modified return type

--- TDD test/main/main.js
+++ TDD test/main/main.js
@@ -1,12 +1,12 @@
 module.exports = function main(km, minute) {
     if(km <= 2) {
-        return "" + Math.round(6 + minute * 0.25);
+        return Math.round(6 + minute * 0.25);
     }
 
     if(km > 2 && km < 8) {
         let result = km * 0.8 + minute * 0.25;
-        return "" + Math.round(result);
+        return Math.round(result);
     }
 
-    return "" + Math.round(6.4 + (km - 8) * 0.8 * (1 + 0.5) + minute * 0.25);
+    return Math.round(6.4 + (km - 8) * 0.8 * (1 + 0.5) + minute * 0.25);
 };
--- TDD test/spec/main-spec.js
+++ TDD test/spec/main-spec.js
@@ -6,21 +6,21 @@ describe('taxi fee', function () {
         let km = 1;
         let minute = 1;
         let result = main(km, minute);
-        expect(result).toEqual("6");
+        expect(result).toEqual(6);
     });
 
     it("More than 2 kilometers, less than 8 kilometers", function() {
         let km = 3;
         let minute = 1;
         let result = main(km, minute);
-        expect(result).toEqual("3");
+        expect(result).toEqual(3);
     });
 
     it("More than 8 kilometers", function() {
         let km = 10;
         let minute = 9;
         let result = main(km, minute);
-        expect(result).toEqual("11");
+        expect(result).toEqual(11);
     });
 
 });
