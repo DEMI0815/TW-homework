0f4457e547472f12cb76b0e267718568528be106
DEMI0815 <807205251@qq.com>
2019-03-22 11:03:12 +0800

    use English rather than Chinese in the code

--- TDD test/spec/main-spec.js
+++ TDD test/spec/main-spec.js
@@ -2,21 +2,21 @@ const main = require('../main/main');
 
 describe('taxi fee', function () {
 
-    it("起步价两公里以内", function() {
+    it("The starting price is within 2 kilometers.", function() {
         let km = 1;
         let minute = 1;
         let result = main(km, minute);
         expect(result).toEqual("6");
     });
 
-    it("两公里以上，8公里以内", function() {
+    it("More than 2 kilometers, less than 8 kilometers", function() {
         let km = 3;
         let minute = 1;
         let result = main(km, minute);
         expect(result).toEqual("3");
     });
 
-    it("八公里以上", function() {
+    it("More than 8 kilometers", function() {
         let km = 10;
         let minute = 9;
         let result = main(km, minute);
